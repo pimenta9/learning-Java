651632ad38ac83beba5c67d6952ff90415336832
joaovitor <joaovfp2002@gmail.com>
2024-05-24 01:51:12 -0300

    first method

--- my-first-java-project/src/MyClass.java
+++ my-first-java-project/src/MyClass.java
@@ -2,6 +2,17 @@ public class MyClass
 {
     public static void main (String[] args)
     {
-        System.out.println("Hello World!");
+        String firstName = "João";
+        String secondName = "Vitor";
+
+        String fullName = fullName(firstName, secondName);
+
+        System.out.println(fullName);
     }
+
+    public static String fullName (String firstName, String secondName)
+    {
+        return firstName.concat(" ").concat(secondName);
+    }
+
 }
